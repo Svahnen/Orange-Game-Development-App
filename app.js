c8f008dd81059843b150e0d3940bc912e05647fc
Svahnen <svahn.jonny@gmail.com>
2017-12-19 13:01:55 +0100

    Fix linter errors in app.js

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-var http = require("http");
+var http = require('http')
 
 http.createServer(function (request, response) {
-
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello from node! (this is the mobie site)\n');
-}).listen(8081);
+  // Send the HTTP header
+  // HTTP Status: 200 : OK
+  // Content Type: text/plain
+  response.writeHead(200, {'Content-Type': 'text/plain'})
+  // Send the response body as "Hello World"
+  response.end('Hello from node! (this is the mobie site)\n')
+}).listen(8081)
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:8081/')
