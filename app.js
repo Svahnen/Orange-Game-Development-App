6a68ad90ea09559d13bf526d2afc24183433f763
Alexosk <alexsigurdar@gmail.com>
2018-02-05 14:00:26 +0100

    Fix create table function

--- app.js
+++ app.js
@@ -32,12 +32,12 @@ app.get('/createdb', (req, res) => {
   })
 })
 // Create Table
-app.get('/createpoststable', (req, res) => {
-  let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))'
+app.get('/createteamstable', (req, res) => {
+  let sql = 'CREATE TABLE teams(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))'
   db.query(sql, (err, result) => {
     if (err) throw err
     console.log('result')
-    res.send('Posts table created')
+    res.send('Teams table created')
   })
 })
 
