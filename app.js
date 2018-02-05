e5fc0e5ce1f7d99333b8b1cda33a985b30959e28
Alexosk <alexsigurdar@gmail.com>
2018-02-05 12:47:12 +0100

    Att functions for database

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ const mysql = require('mysql')
 // Create connection
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'debian-sys-maint',
-  password: 'PTg2JrAD6bLfrVEU',
+  user: 'root',
   database: 'nodemysql'
 })
 
@@ -23,32 +22,91 @@ db.connect((err) => {
 
 const app = express()
 
-/*db.query('SELECT * FROM clues', (err, rows) => {
-  if (err) throw err
-
-  console.log('Data received from Db:\n')
-  console.log(rows)
-  rows.forEach((row) => {
-    console.log(`${row.title} is in ${row.clue}`)
-  })
-})*/
-
-
- const team = {name: 'Team Horses', score: '10:22'}
- db.query('INSERT INTO teams SET ?', team, (err, res) => {
-   if (err) throw err
-   console.log('Last insert ID:', res.insertId)
- })
-
- db.query(
-  'DELETE FROM teams WHERE ID = ?', [1], (err, result) => {
+// Add new team
+let teamName = 'Team Anna'
+let endScore = '23:33'
+app.get('/addteam', (req, res) => {
+  let team = {name: teamName, score: endScore}
+  let sql = 'INSERT INTO teams SET ?'
+  let query = db.query(sql, team, (err, result) => {
     if (err) throw err
-    console.log(`Deleted ${result.affectedRows}row(s)`)
-  }
-)
+    console.log('result')
+    res.send(teamName + ' won with ' + endScore)
+  })
+})
+// Select singel team
+app.get('/getteam/:id', (req, res) => {
+  let sql = `SELECT * FROM teams WHERE id = ${req.params.id}`
+  let query = db.query(sql, (err, result) => {
+    if (err) throw err
+    console.log('result')
+    res.send(teamName + ' fetched, with id number ' + `${req.params.id}`)
+  })
+})
+// Get all team information
+app.get('/getteams', (req, res) => {
+  let sql = 'SELECT * FROM teams'
+  let query = db.query(sql, (err, results) => {
+    if (err) throw err
+    // console.log(results)
+    let content = 'These are the teams and their scores: <br><br>'
+    results.forEach((row) => {
+      content += `${row.name} finished in ${row.score} <br>`
+    })
+    res.send(content)
+  })
+})
+// Delete a team
+app.get('/deleteteam/:id', (req, res) => {
+  let sql = `DELETE FROM teams WHERE id = ${req.params.id}`
+  let query = db.query(sql, (err, result) => {
+    if (err) throw err
+    console.log('result')
+    res.send('Team with id number ' + `${req.params.id}` + ' has been Deleted')
+  })
+})
+// Add new clue
+app.get('/addclue', (req, res) => {
+  let clue = {title: 'Clue #12', clue: 'answer[11]'}
+  let sql = 'INSERT INTO clues SET ?'
+  let query = db.query(sql, clue, (err, result) => {
+    if (err) throw err
+    console.log('result')
+    res.send('Clue added')
+  })
+})
+// Get Random clues
+app.get('/getclues', (req, res) => {
+  let sql = 'SELECT clue FROM clues ORDER BY RAND() LIMIT 5'
+  let query = db.query(sql, (err, results) => {
+    if (err) throw err
+    // console.log(results)
+    res.send(results)
+    let content = 'These are clues selected randomly: <br>'
+    results.forEach((row) => {
+      content += `${row.name} is in ${row.clue} with id ${row.id}<br>`
+    })
+    res.send(content)
+  })
+})
 
-// Create DB
-/* app.get('/createdb', (req, res) => {
+// Change team name
+app.get('/updateteamname/:id', (req, res) => {
+  let newName = 'Team Blue'
+  let sql = `UPDATE teams SET name = '${newName}' WHERE id = ${req.params.id}`
+  let query = db.query(sql, (err, result) => {
+    if (err) throw err
+    console.log('result')
+    res.send('Team name has been changed to ' + `${newName}`)
+  })
+})
+
+app.listen('3000', () => {
+  console.log('Server running on port: 3000')
+})
+
+/* // Create DB
+app.get('/createdb', (req, res) => {
   let sql = 'CREATE DATABASE nodemysql'
   db.query(sql, (err, result) => {
     if (err) throw err
@@ -64,74 +122,4 @@ app.get('/createpoststable', (req, res) => {
     console.log('result')
     res.send('Posts table created')
   })
-})
-// Add new team
-
-// insert post 1
-app.get('/addpost', (req, res) => {
-  let post = {title: 'Post One', body: 'This is post number one'}
-  let sql = 'INSERT INTO posts SET ?'
-  let query = db.query(sql, post, (err, result) => {
-    if (err) throw err
-    console.log('result')
-    res.send('Post 1 added')
-  })
-})
-// Make another post
-app.get('/addpost2', (req, res) => {
-  let post = {title: 'Post two', body: 'This is post number two'}
-  let sql = 'INSERT INTO posts SET ?'
-  let query = db.query(sql, post, (err, result) => {
-    if (err) throw err
-    console.log('result')
-    res.send('Post 2 added')
-  })
-})
-
-// Select posts
-app.get('/getposts', (req, res) => {
-  let sql = 'SELECT * FROM posts'
-  let query = db.query(sql, (err, results) => {
-    if (err) throw err
-    // console.log(results)
-    res.send(results)
-    /* let html = 'This is the rows: <br>'
-    results.forEach((row) => {
-      html += `${row.title} is in ${row.body} with id ${row.id}<br>`
-    })
-    res.send(html) */
-  })
-})
-// Select singel post
-/*app.get('/getpost/:id', (req, res) => {
-  let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`
-  let query = db.query(sql, (err, result) => {
-    if (err) throw err
-    console.log('result')
-    res.send('Post updated')
-  })
-})
-
-// Update post
-app.get('/updatepost/:id', (req, res) => {
-  let newTitle = 'Updated Title'
-  let sql = `UPDATE posts SET title = '${newTitle}' WHERE id = ${req.params.id}`
-  let query = db.query(sql, (err, result) => {
-    if (err) throw err
-    console.log('result')
-    res.send('Post updated')
-  })
-})
-// Delete post
-app.get('/deletepost/:id', (req, res) => {
-  let sql = `DELETE FROM posts WHERE id = ${req.params.id}`
-  let query = db.query(sql, (err, result) => {
-    if (err) throw err
-    console.log('result')
-    res.send('Post Deleted')
-  })
-})
-
-app.listen('8080', () => {
-  console.log('Server running on port: 8080')
-})*/
+}) */
